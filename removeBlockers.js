44a7db97601cc77d912a86c0b364c4bc19f23d0f
Stephen <stephen.anthony.rivas@gmail.com>
2024-04-29 23:56:47 -0700

    added teamblind to hosts list and added forked logic to apply styles for teamblind or glassdoor

--- removeBlockers.js
+++ removeBlockers.js
@@ -1,50 +1,84 @@
 /* removeBlockers.js
 
-- Hide obstructive elements from glassdoor.com by overriding styles
+- Hide obstructive elements from glassdoor.com and teamblind.com by overriding styles
 - Block restrictive scroll functions from preventing page navigation
 
 */
 
-hideObstructions();
+console.log('Clear Glass loaded.');
 
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', preventScrollLock);
-} else {
-    preventScrollLock();
+const url = window.location.href;
+
+if (url.includes('glassdoor')) {
+    console.log('Removing blockers from Glassdoor');
+    removeBlockersGlassdoor();
+} else if (url.includes('teamblind')) {
+    console.log('Removing blockers from Blind');
+    removeBlockersBlind();
 }
 
-function hideObstructions() {
+// === GLASSDOOR ===
+function removeBlockersGlassdoor() {
+    hideObstructions();
+
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', preventScrollLock);
+    } else {
+        preventScrollLock();
+    }
+
+    function hideObstructions() {
+        const styleElement = document.createElement('style');
+
+        styleElement.textContent = `
+        body {
+            overflow: visible !important;
+            position: static !important;
+        }
+        div#ContentWallHardsell {
+            display: none !important;
+        }
+        div#HardsellOverlay {
+            display: none !important;
+        }
+        div#UserAlert {
+            display: none !important;
+        }
+        `;
+
+        document.head.prepend(styleElement);
+    }
+
+    function preventScrollLock() {
+        const hardsellLoggedIn = document.getElementById('ContentWallHardsell');
+
+        if (hardsellLoggedIn) {
+            window.addEventListener(
+                'scroll',
+                (e) => {
+                    e.stopPropagation();
+                },
+                { capture: true },
+            );
+        }
+    }
+}
+
+// === BLIND ===
+function removeBlockersBlind() {
     const styleElement = document.createElement('style');
 
     styleElement.textContent = `
-    body {
-        overflow: visible !important;
-        position: static !important;
-    }
-    div#ContentWallHardsell {
-        display: none !important;
-    }
-    div#HardsellOverlay {
-        display: none !important;
-    }
-    div#UserAlert {
-        display: none !important;
-    }
-    `;
+        body {
+            overflow: visible !important;
+        }
+        div.backdrop-blur-sm {
+            display: none !important;
+        }
+        div.z-20 {
+            display: none !important;
+        }
+        `;
 
     document.head.prepend(styleElement);
-}
-
-function preventScrollLock() {
-    const hardsellLoggedIn = document.getElementById('ContentWallHardsell');
-
-    if (hardsellLoggedIn) {
-        window.addEventListener(
-            'scroll',
-            (e) => {
-                e.stopPropagation();
-            },
-            { capture: true },
-        );
-    }
 }
